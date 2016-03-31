adff0eebbebd86870a42f6fdf6c906b69c539659
lunete <claud@openmailbox.org>
2016-03-31 01:32:25 -0400

    added collision then made gaps wider again because it was too hard

--- main.js
+++ main.js
@@ -6,32 +6,50 @@ var mainstate = {
     game.load.image('player', 'assets/fhazer_player_50.png');
     game.load.image('wall', 'assets/obstacle.png');
   },
+
+
   create: function() {
     game.stage.backgroundColor = '#2e62c7';
+
     // start the physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
+
     // player stuff
     this.player = game.add.sprite(100, 245, 'player');
     game.physics.arcade.enable(this.player);
     this.player.body.gravity.y = 1000;
-    // other stuff
+
+    // wall stuff
     this.walls = game.add.group();
     // add a wall every 1.5 seconds
     this.timer = game.time.events.loop(1500, this.addRowWallBlock, this);
+
+    // score
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, '0',
+      {font: '30px Helvetica', fill: '#ffffff'});
+
     // controls
     var spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spacekey.onDown.add(this.jump, this);
   },
+
+
   update: function() {
     // reset if player is too higher or too low on screen
     if(this.player.y < 0 || this.player.y > 490) this.restartGame();
+    game.physics.arcade.overlap(this.player, this.walls, this.restartGame, null, this);
   },
+
+
   jump: function() {
     this.player.body.velocity.y = -350;
   },
+
   restartGame: function() {
     game.state.start('main');
   },
+
   addOneWallBlock: function(x, y) {
     var wall = game.add.sprite(x, y, 'wall');
     this.walls.add(wall);
@@ -41,10 +59,11 @@ var mainstate = {
     wall.checkWorldBounds = true;
     wall.outOfBoundsKill = true;
   },
+
   addRowWallBlock: function() {
     var gap = Math.floor(Math.random() * 5) + 1;
     for(var i = 0; i < 8; ++i) {
-      if(i != gap && i != gap + 1) {
+      if(i != gap && i != gap + 1 && i != gap + 2) {
         this.addOneWallBlock(400, i * 60 + 10);
       }
     }

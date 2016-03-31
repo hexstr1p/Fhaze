4059dbf576941444b69dda36f5ac62eb25c8d686
lunete <claud@openmailbox.org>
2016-03-31 01:21:01 -0400

    added wall then made player smaller because it was too hard

--- main.js
+++ main.js
@@ -3,17 +3,22 @@ var game = new Phaser.Game(400, 490);
 
 var mainstate = {
   preload: function() {
-    game.load.image('player', 'assets/fhazer_player_70.png');
+    game.load.image('player', 'assets/fhazer_player_50.png');
+    game.load.image('wall', 'assets/obstacle.png');
   },
   create: function() {
     game.stage.backgroundColor = '#2e62c7';
     // start the physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
+    // player stuff
     this.player = game.add.sprite(100, 245, 'player');
-    // player.scale.setTo(70, 70);
     game.physics.arcade.enable(this.player);
     this.player.body.gravity.y = 1000;
-
+    // other stuff
+    this.walls = game.add.group();
+    // add a wall every 1.5 seconds
+    this.timer = game.time.events.loop(1500, this.addRowWallBlock, this);
+    // controls
     var spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spacekey.onDown.add(this.jump, this);
   },
@@ -26,6 +31,23 @@ var mainstate = {
   },
   restartGame: function() {
     game.state.start('main');
+  },
+  addOneWallBlock: function(x, y) {
+    var wall = game.add.sprite(x, y, 'wall');
+    this.walls.add(wall);
+    game.physics.enable(wall);
+    wall.body.velocity.x = -200;
+    // auto destroy wall when it leaves
+    wall.checkWorldBounds = true;
+    wall.outOfBoundsKill = true;
+  },
+  addRowWallBlock: function() {
+    var gap = Math.floor(Math.random() * 5) + 1;
+    for(var i = 0; i < 8; ++i) {
+      if(i != gap && i != gap + 1) {
+        this.addOneWallBlock(400, i * 60 + 10);
+      }
+    }
   }
 };
 
